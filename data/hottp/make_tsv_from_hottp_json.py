423b035b2ea01724ee623a6d081ab97fb6187fa4
ryderwishart <ryderwishart@gmail.com>
2023-10-24 16:22:41 -0700

    Handle single ref rows

--- data/hottp/make_tsv_from_hottp_json.py
+++ data/hottp/make_tsv_from_hottp_json.py
@@ -19,7 +19,11 @@ def process_json_file(json_file):
         with open(output_file, 'a') as f:
             # write the refs array, \t, and then a json dump of the entry
             entry_json = json.dumps(entry, ensure_ascii=False)
-            f.write(f'{refs}\t{entry_json}\n')
+            
+            # sometimes refs will be a string, and sometimes an array. It should always be an array
+            ref_value = refs if isinstance(refs, list) else [refs]
+            
+            f.write(f'{ref_value}\t{entry_json}\n')
 
 for file in os.listdir('.'):
     if file.endswith('.json'):
