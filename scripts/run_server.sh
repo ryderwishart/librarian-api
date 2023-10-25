f398d26a183ff0c834726aff31b3867245a43e73
ryderwishart <ryderwishart@gmail.com>
2023-10-25 07:44:35 -0700

    Improve start script

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+echo "Setting up virtual environment..."
 # Activate the virtual environment
 source api_venv/bin/activate
 
+echo "Attempting to stop any running ClickHouse and Gunicorn processes..."
+# Kill any running ClickHouse and Gunicorn processes
+pkill -f clickhouse
+pkill -f gunicorn
+
+echo "Starting ClickHouse server..."
 # Start ClickHouse server in the background, redirecting output to /dev/null
 nohup ../../clickhouse server >/dev/null 2>&1 &
 
+# Wait for ClickHouse server to start
+echo "Waiting for ClickHouse server to start..."
+while ! curl --output /dev/null --silent --head --fail http://localhost:8123; do sleep 1 && echo -n .; done;
+echo "ClickHouse server started."
+
+echo "Starting WSGI server..."
 # Start the Flask application with Gunicorn in the background, redirecting output to /dev/null
 nohup gunicorn -w 4 -b 0.0.0.0:5000 server_with_tables:app >/dev/null 2>&1 &
+# Wait for the WSGI server to start
+echo "Waiting for WSGI server to start..."
+while ! curl --output /dev/null --silent --head --fail http://localhost:5000; do sleep 1 && echo -n .; done;
+echo "API server started on port 5000."
 
 # Optionally, you can redirect output to log files instead of /dev/null for debugging purposes:
 # nohup ./clickhouse server > clickhouse.log 2>&1 &
