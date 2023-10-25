6ec8777582e59b1399eca45007c6d1711aaff79b
ryderwishart <ryderwishart@gmail.com>
2023-10-25 08:37:33 -0700

    Remove waiting for WSGI server

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -22,10 +22,10 @@ echo "Starting WSGI server..."
 # Start the Flask application with Gunicorn in the background, redirecting output to /dev/null
 nohup gunicorn -w 4 -b 0.0.0.0:5000 server_with_tables:app >/dev/null 2>&1 &
 # Wait for the WSGI server to start
-echo "Waiting for WSGI server to start..."
-while ! curl --output /dev/null --silent --head --fail 'http://localhost:5000/query?file=macula/macula-with-marble-ids.tsv&search_string=ROM 1:9&column_name=VREF&limit=3'; do sleep 1 && echo -n .; done
+# echo "Waiting for WSGI server to start..."
+# while ! curl --output /dev/null --silent --head --fail 'http://localhost:5000/query?file=macula/macula-with-marble-ids.tsv&search_string=ROM 1:9&column_name=VREF&limit=3'; do sleep 1 && echo -n .; done
+sleep 3
 echo "API server started on port 5000."
-
 # Optionally, you can redirect output to log files instead of /dev/null for debugging purposes:
 # nohup ./clickhouse server > clickhouse.log 2>&1 &
 # nohup gunicorn -w 4 -b 0.0.0.0:5000 server_with_tables:app > gunicorn.log 2>&1 &
