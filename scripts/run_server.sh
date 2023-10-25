85bec81c7cc9969c4e14caeb8f107fd34e6f1488
ryderwishart <ryderwishart@gmail.com>
2023-10-25 08:50:48 -0700

    Add directory navigation

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-echo "Setting up virtual environment..."
+# Should be run from librarian-api/scripts/
+
+echo -e "\e[32mSetting up virtual environment...\e[0m"
 # Activate the virtual environment
 source api_venv/bin/activate
 
-echo "Attempting to stop any running ClickHouse and Gunicorn processes..."
+echo -e "\e[32mAttempting to stop any running ClickHouse and Gunicorn processes...\e[0m"
 # Kill any running ClickHouse and Gunicorn processes
 pkill -f clickhouse
 pkill -f gunicorn
 
-echo "Starting ClickHouse server..."
+echo -e "\e[32mStarting ClickHouse server...\e[0m"
 # Start ClickHouse server in the background, redirecting output to /dev/null
-nohup ../../clickhouse server >/dev/null 2>&1 &
+cd ../..
+nohup ./clickhouse server >/dev/null 2>&1 &
 
 # Wait for ClickHouse server to start
-echo "Waiting for ClickHouse server to start..."
+echo -e "\e[32mWaiting for ClickHouse server to start...\e[0m"
 while ! curl --output /dev/null --silent --head --fail http://localhost:8123; do sleep 1 && echo -n .; done
-echo "ClickHouse server started."
+echo -e "\e[32mClickHouse server started.\e[0m"
 
-echo "Starting WSGI server..."
+cd librarian-api/scripts/
+
+echo -e "\e[32mStarting WSGI server...\e[0m"
 # Start the Flask application with Gunicorn in the background, redirecting output to /dev/null
 nohup gunicorn -w 4 -b 0.0.0.0:5000 server_with_tables:app >/dev/null 2>&1 &
 # Wait for the WSGI server to start
 # echo "Waiting for WSGI server to start..."
 # while ! curl --output /dev/null --silent --head --fail 'http://localhost:5000/query?file=macula/macula-with-marble-ids.tsv&search_string=ROM 1:9&column_name=VREF&limit=3'; do sleep 1 && echo -n .; done
 sleep 3
-echo "API server started on port 5000."
+echo -e "\e[32mAPI server started on port 5000.\e[0m"
 # Optionally, you can redirect output to log files instead of /dev/null for debugging purposes:
 # nohup ./clickhouse server > clickhouse.log 2>&1 &
 # nohup gunicorn -w 4 -b 0.0.0.0:5000 server_with_tables:app > gunicorn.log 2>&1 &
